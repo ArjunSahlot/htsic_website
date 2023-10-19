d5a88810e170ca55c8f3924eb7f70d2d1c12b6c9
ArjunSahlot <iarjun.sahlot@gmail.com>
2023-10-19 11:36:04 -0700

    multi page setup

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
-import Home from "./pages/Home";
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import logo from "./logo.svg";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Get from "./pages/Get";
+
 import "./App.css";
 
 function App() {
 	return (
-		<div className="App">
-			<Home />
-		</div>
+		<BrowserRouter>
+			<div className="App">
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/about" element={<About />} />
+					<Route path="/contact" element={<Contact />} />
+					<Route path="/get" element={<Get />} />
+				</Routes>
+			</div>
+		</BrowserRouter>
 	);
 }
 
